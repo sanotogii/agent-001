e447033b16db2f62daf949db28595171dd3aca92
Khalid-lazrag <khalid.lazrag@etu.uae.ac.ma>
2026-09-21 20:55:54 +0000

    feat: User input parse

---
+++ main.py
@@ -0,0 +1,35 @@
+import os, argparse
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
+
+# print(f"User prompt: {args.user_prompt}")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key)
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": args.user_prompt
+        }
+    ])
+
+# Prompt tokens: X
+# Response tokens: Y
+print("User prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(f"Prompt tokens: {response.usage.prompt_tokens}")
+print(f"Response tokens: {response.usage.completion_tokens}")
+
+print("Response:")
+print(response.choices[0].message.content)
